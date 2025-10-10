839e40a92dd431c4aac4d15c33fea04e5a0bd9d7
balaji379 <vigneshbalaji.kvl@gmail.com>
2025-10-10 09:51:18 +0530

    project-creation

---
+++ grpc-jwt/grpc-jwt/src/main/java/com/vb/grpc_jwt/GrpcJwtApplication.java
@@ -0,0 +1,13 @@
+package com.vb.grpc_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcJwtApplication.class, args);
+	}
+
+}
