bd0acd659d12b5bc06021c27074999f6406d1b83
balaji379 <vigneshbalaji.kvl@gmail.com>
2025-11-07 09:53:16 +0530

    project folder creation

---
+++ excel-batch-application/src/main/java/com/tutorial/excel_batch_application/ExcelBatchApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.excel_batch_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelBatchApplication.class, args);
+	}
+
+}
---
+++ excel-batch-application/src/test/java/com/tutorial/excel_batch_application/ExcelBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.excel_batch_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
